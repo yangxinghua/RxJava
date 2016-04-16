8c09e5ef6a721a87dd30a8c030a5caf9b448c2ce
ivan <yxh.ivan@gmail.com>
2016-04-16 22:19:30 +0800

    add rxjava map operation

--- RxJavaSample/app/src/main/java/com/ivan/rxjavasample/MainActivity.java
+++ RxJavaSample/app/src/main/java/com/ivan/rxjavasample/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -20,6 +19,7 @@ import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,47 +27,28 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final String IMAGE_PATH = "http://img.my.csdn.net/uploads/201407/26/1406383299_1976.jpg";
 
-    @Bind(R.id.image_view)
-    ImageView mImageView;
+    @Bind(R.id.image_left)
+    ImageView mImageLeft;
+
+    @Bind(R.id.image_right)
+    ImageView mImageRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         downloadAndDisplayImage();
+        map();
     }
 
     private void downloadAndDisplayImage() {
         Observable.create(new Observable.OnSubscribe<Bitmap>() {
             @Override
             public void call(Subscriber<? super Bitmap> subscriber) {
-                InputStream is = null;
-                try {
-                    URL mUrl = new URL(IMAGE_PATH);
-                    HttpURLConnection connection = (HttpURLConnection) mUrl.openConnection();
-                    connection.setDoInput(true);
-                    connection.connect();
-                    is = connection.getInputStream();
-                    final BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inSampleSize = 4;
-                    // Decode bitmap with inSampleSize set
-                    options.inJustDecodeBounds = false;
-                    Bitmap bitmap = BitmapFactory.decodeStream(is, null, options);
-                    Log.d(TAG, "bitmap >>>>> " + bitmap);
-                    subscriber.onNext(bitmap);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        is.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
+                Bitmap bitmap = downloadBitmap(IMAGE_PATH);
+                subscriber.onNext(bitmap);
             }
         }).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -85,10 +66,67 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onNext(Bitmap bitmap) {
-                        mImageView.setImageBitmap(bitmap);
+                        mImageLeft.setImageBitmap(bitmap);
                     }
                 });
 
 
+    }
+
+    private void map() {
+        Observable.just(IMAGE_PATH)
+                .map(new Func1<String, Bitmap>() {
+                    @Override
+                    public Bitmap call(String s) {
+                        return downloadBitmap(s);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Bitmap>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Bitmap bitmap) {
+                        mImageRight.setImageBitmap(bitmap);
+                    }
+                });
+    }
+
+    private Bitmap downloadBitmap(String path) {
+        InputStream is = null;
+        try {
+            URL mUrl = new URL(IMAGE_PATH);
+            HttpURLConnection connection = (HttpURLConnection) mUrl.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            is = connection.getInputStream();
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 4;
+            // Decode bitmap with inSampleSize set
+            options.inJustDecodeBounds = false;
+            Bitmap bitmap = BitmapFactory.decodeStream(is, null, options);
+            return bitmap;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
     }
 }
